e4a2d97223c3fa69d5accc3dcffc9ce1ab32bd9e
Kenny Pflug <kenny.pflug@live.de>
2025-06-29 10:48:15 +0200

    docs: adjust XML comments for ITemporaryStreamService
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- Light.TemporaryStreams/ITemporaryStreamService.cs
+++ Light.TemporaryStreams/ITemporaryStreamService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 
 namespace Light.TemporaryStreams;
@@ -13,20 +14,25 @@ public interface ITemporaryStreamService
     /// </para>
     /// </summary>
     /// <param name="expectedLengthInBytes">
-    /// The length of the stream. Will be used to determine whether a file stream or a memory stream should be used.
+    /// The length of the source stream whose contents will be copied to the new temporary stream. This value will be
+    /// used to determine whether a file stream or a memory stream should be used.
     /// </param>
     /// <param name="filePath">
-    /// The optional file path. If not provided, <see cref="Path.GetTempFileName" /> will be used to create a temporary
-    /// file. Please ensure that you have appropriate options configured if you want to open an existing file instead
-    /// of creating a new one or overwriting an existing one.
+    /// The optional file path. If not provided, a temporary file (usually created via
+    /// <see cref="Path.GetTempFileName" />) will be used to create a temporary file. Please ensure that you have
+    /// appropriate options configured if you want to open an existing file instead of creating a new one or overwriting
+    /// an existing one.
     /// </param>
     /// <param name="options">
-    /// The optional options. If not provided, the <see cref="TemporaryStream" /> passed via the constructor will be used. This
+    /// The optional options. If not provided, the options passed via the constructor will be used. This
     /// object allows you to deviate from the default options which are optimized for creating new temporary files. By
     /// adapting an options object, you can open existing files, change the buffer size, or the file options. You can
     /// also specify the threshold when a file stream is used instead of a memory stream.
     /// </param>
-    /// <returns>The created <see cref="TemporaryStreamService" /> instance.</returns>
+    /// <returns>The created <see cref="TemporaryStream" /> instance.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Thrown when <paramref name="expectedLengthInBytes" /> is less than 0.
+    /// </exception>
     TemporaryStream CreateTemporaryStream(
         long expectedLengthInBytes,
         string? filePath = null,
