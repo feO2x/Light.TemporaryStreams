ccfc727dd905674278d9226d9264e283c5bbc8ef
Kenny Pflug <kenny.pflug@live.de>
2025-07-06 00:15:24 +0200

    test: add additional test for HashingPlugin.AfterCopyAsync
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- tests/Light.TemporaryStreams.Core.Tests/Hashing/HashingPluginTests.cs
+++ tests/Light.TemporaryStreams.Core.Tests/Hashing/HashingPluginTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Immutable;
+﻿using System;
+using System.Collections.Immutable;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -34,5 +35,17 @@ public static class HashingPluginTests
     {
         using var sha1 = SHA1.Create();
         await using var hashingPlugin = new HashingPlugin([sha1], disposeCalculators: false);
+    }
+
+    [Fact]
+    public static async Task AfterCopyAsync_ShouldThrow_WhenSetUpAsyncWasNotCalledBeforehand()
+    {
+        await using var hashingPlugin = new HashingPlugin([SHA1.Create()]);
+
+        // ReSharper disable once AccessToDisposedClosure -- delegate called before hashingPlugin is disposed of
+        var act = () => hashingPlugin.AfterCopyAsync().AsTask();
+
+        (await act.Should().ThrowAsync<InvalidOperationException>())
+           .Which.Message.Should().Be("SetUpAsync must be called before AfterCopyAsync");
     }
 }
